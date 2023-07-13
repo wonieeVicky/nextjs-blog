97db3217bd6a6e362b0d2dd47380b894d6a26a74
wonieeVicky <hwfongfing@gmail.com>
2023-07-13 23:45:25 +0900

    feat: layout 내 header 영역 추가(타이틀, topnav)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,44 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Nanum_Gothic, Open_Sans } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Vicky's Blog",
   description: 'Generated Blog by next app'
 };
 
+const navigation = [
+  { name: 'home', href: '/' },
+  { name: 'about', href: '/about' },
+  { name: 'products', href: 'products' },
+  { name: 'contact', href: 'contact' }
+];
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={gothic.className}>
+        <header>
+          <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <div className="flex lg:flex-1">
+              <h1 className={gothic.className}>Vicky's blog</h1>
+            </div>
+            <div className="flex gap-x-12">
+              {navigation.map((item) => (
+                <Link key={item.name} href={item.href}>
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
