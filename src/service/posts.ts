c5dcce812a86ec6eb7cbd9fc2c80d5f8d0caf509
wonieeVicky <hwfongfing@gmail.com>
2023-07-16 16:26:24 +0900

    feat: getData 함수 구현(getPosts, getPost)

---
+++ src/service/posts.ts
@@ -0,0 +1,22 @@
+﻿import path from 'path';
+import { promises as fs } from 'fs';
+
+export type Post = {
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  path: string;
+  featured: boolean;
+};
+
+export async function getPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), 'data', 'posts.json');
+  const data = await fs.readFile(filePath, 'utf-8');
+  return JSON.parse(data);
+}
+
+export async function getPost(path: string): Promise<Post | undefined> {
+  const posts = await getPosts();
+  return posts.find((post) => post.path === path);
+}
