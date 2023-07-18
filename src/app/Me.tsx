962e976489ba84a29b61ac598ecf88e7723dfe08
wonieeVicky <hwfongfing@gmail.com>
2023-07-18 20:20:08 +0900

    feat: About page 구현

--- src/app/Me.tsx
+++ src/app/Me.tsx
@@ -8,8 +8,8 @@ export default function Me() {
       <div className="border w-52 h-52 border-radius overflow-hidden rounded-full mb-3 inline-block">
         <Image src={meImage} alt="me image" className="relative -top-3" priority />
       </div>
-      <h2 className="text-xl font-bold mb-1">Hi! I&apos;m Vicky</h2>
-      <p className="text-sm mb-3">Front-end engineer</p>
+      <h2 className="text-xl font-bold mb-1">Hi! I&apos;m Vicky 👩🏻‍💻</h2>
+      <p className="text-sm mb-3">Creating delightful experiences</p>
       <button className="rounded-full bg-orange-500 p-1 px-3 text-xs font-semibold text-white">
         <Link href="/about">Contact Me</Link>
       </button>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,23 @@
 
 export default function About() {
   return (
-    <main className="flex flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between lg:px-8 p-6">
       <Me />
+      <div className="w-full bg-yellow-50 text-center p-6 shadow">
+        <h3 className="text-xl font-semibold my-3">Who am I?</h3>
+        <div className="text-sm">비즈니스의 가치를 구현하는 프론트엔드 개발자</div>
+        <h3 className="text-xl font-semibold my-3 mt-5">Career</h3>
+        <div className="text-sm">(주)유니드컴즈 (2018 ~ Now)</div>
+        <h3 className="text-xl font-semibold my-3 mt-5">Education & Certificate</h3>
+        <div className="text-sm">한국방송통신대학교 컴퓨터과학과 (2023.08)</div>
+        <div className="text-sm">-</div>
+        <div className="text-sm">정보처리기사 (2023.06)</div>
+        <div className="text-sm">웹디자인기능사 (2018.06)</div>
+        <div className="text-sm mb-5">그래픽기술자격(GTQ), 그래픽기술자격일러스트(GTQi) (2017.09)</div>
+        <h3 className="text-xl font-semibold my-3 mt-5">Skill</h3>
+        <div className="text-sm">React, Node, Next.js, Typescript, Javascript, Three.js</div>
+        <div className="text-sm">Git, VsCode, IntelliJ, Sourcetree, Webpack</div>
+      </div>
     </main>
   );
 }
