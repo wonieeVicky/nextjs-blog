f781696b06bcc999c61edda37d5d1c0c7ac9b951
wonieeVicky <hwfongfing@gmail.com>
2023-07-19 23:50:55 +0900

    feat: CSR로 PostList 구현 후 post page 내 렌더링

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,14 @@
+﻿import { getPosts } from '@/service/posts';
+import PostList from '@/components/PostList';
+
+export default async function PostPage() {
+  const posts = await getPosts();
+
+  return (
+    <>
+      <div className="flex flex-row">
+        <PostList posts={posts} />
+      </div>
+    </>
+  );
+}
