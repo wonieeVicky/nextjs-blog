0ca590e0dd3d564433bf02de2f9d51e09c3b3f54
wonieeVicky <hwfongfing@gmail.com>
2023-07-20 23:50:21 +0900

    feat: service에 getPostMarkdown 구현

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -20,3 +20,9 @@ export async function getPost(path: string): Promise<Post | undefined> {
   const posts = await getPosts();
   return posts.find((post) => post.path === path);
 }
+
+export async function getPostMarkdown(markdownPath: string): Promise<string> {
+  const filePath = path.join(process.cwd(), 'data/posts', `${markdownPath}.md`);
+  const data = await fs.readFile(filePath, 'utf-8');
+  return data;
+}
