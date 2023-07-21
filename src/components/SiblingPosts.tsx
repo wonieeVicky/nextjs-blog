a29d46e7b46d981347343809a405de3cec3be366
wonieeVicky <hwfongfing@gmail.com>
2023-07-21 23:34:04 +0900

    feat: service에 getSiblingPosts 함수 구현 및 SiblingPosts CSR 컴포넌트 구현(상세페이지 내 다른 게시글 연결)

---
+++ src/components/SiblingPosts.tsx
@@ -0,0 +1,29 @@
+﻿'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { Post } from '@/service/posts';
+
+type Props = {
+  data: Post[];
+};
+
+export default function SiblingPosts({ data }: Props) {
+  return (
+    <>
+      <div className="border flex">
+        {data.map((post, idx) => (
+          <div key={idx} className="text-center h-48 overflow-hidden relative">
+            <Link href={`/posts/${post.path}`} className="inline-block h-full overflow-hidden">
+              <Image src={`/images/posts/${post.path}.png`} className="brightness-50" width={800} height={300} alt={`${post.path} image`} />
+              <div className="absolute w-full top-0 h-full flex flex-col justify-center text-slate-50">
+                <h5 className="mb-2 font-bold text-2xl tracking-tight">{post.title}</h5>
+                <p className="font-light">{post.description}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -21,6 +21,14 @@ export async function getPost(path: string): Promise<Post | undefined> {
   return posts.find((post) => post.path === path);
 }
 
+export async function getSiblingPosts(path: string): Promise<Post[] | undefined> {
+  const posts = await getPosts();
+  const idx = posts.findIndex((post) => post.path === path);
+  const prev = posts[idx - 1] || posts[posts.length - 1];
+  const next = posts[idx + 1] || posts[0];
+  return [prev, next];
+}
+
 export async function getPostMarkdown(markdownPath: string): Promise<string> {
   const filePath = path.join(process.cwd(), 'data/posts', `${markdownPath}.md`);
   const data = await fs.readFile(filePath, 'utf-8');
