b2ec00b54d64778c714d4dfbb756a4c6c7c0f8e5
wonieeVicky <hwfongfing@gmail.com>
2023-07-24 20:05:09 +0900

    feat: SendEmail form 레이아웃 및 yup을 통한 form 유효성 검증(input validation) 구현

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,27 @@
-﻿export default function About() {
-  return <>Contact</>;
+﻿import SendEmail from '@/components/SendEmail';
+import Link from 'next/link';
+import { SiGithub, SiNotion } from 'react-icons/si';
+
+export default function About() {
+  return (
+    <main className="flex flex-col items-center justify-between">
+      <div className="w-full text-center p-6">
+        <h3 className="text-2xl font-bold my-3">Contact Me</h3>
+        <div className="text-sm">hwfongfing@gmail.com</div>
+        <div className="flex justify-center my-5 text-2xl hover:text">
+          <Link href="//github.com/wonieeVicky" target="_blank" className="mx-1 transition ease-in-out duration-300 hover:text-orange-600">
+            <SiGithub />
+          </Link>
+          <Link
+            href="//fongfing.notion.site/Vicky-s-FE-Engineering-Wiki-d7e660205c0047118a78d664b07418fd"
+            target="_blank"
+            className="mx-1 transition ease-in-out duration-200 hover:text-orange-600"
+          >
+            <SiNotion />
+          </Link>
+        </div>
+        <SendEmail />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/SendEmail.tsx
@@ -0,0 +1,59 @@
+﻿'use client';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(''),
+    subject: yup.string().required(),
+    message: yup.string().required()
+  })
+  .required();
+
+export default function SendEmail() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="py-4 mt-8 min-w-[50%]">
+          <h2 className="text-2xl font-bold mb-10">Or Send me an email📮</h2>
+          <form className="grid grid-cols-1 gap-6 text-left" onSubmit={handleSubmit(onSubmit)}>
+            <label className="block">
+              <span>Your Email</span>
+              <input className="mt-1 block w-full rounded-md" placeholder="john@example.com" {...register('email')} />
+              <p className="text-xs mt-1">{errors.email?.message}</p>
+            </label>
+            <label className="block">
+              <span>Subject</span>
+              <input type="text" className="mt-1 block w-full rounded-md" placeholder="" {...register('subject')} />
+              <p className="text-xs mt-1">{errors.subject?.message}</p>
+            </label>
+            <label className="block">
+              <span className="text-gray-700">Message</span>
+              <textarea className="mt-1 block w-full rounded-md" rows={3} {...register('message')}></textarea>
+              <p className="text-xs mt-1">{errors.message?.message}</p>
+            </label>
+            <button
+              type="submit"
+              className="rounded-md py-3 bg-orange-500 font-semibold text-white text-sm transition ease-in-out duration-300 hover:bg-orange-600"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
