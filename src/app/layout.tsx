e312eb24566f8c4fac75edd425657fd95e6dbb76
Vicky <hwfongfing@gmail.com>
2023-07-30 15:39:37 +0900

    fix: About page style & CI/CD test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,19 @@ const navigation = [
   { name: 'contact', href: 'contact' }
 ];
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={(sans.className, 'mb-32')}>
         <header className="sticky top-0 right-0 left-0 bg-white">
-          <nav className="flex items-center justify-between p-6 lg:px-8 max-w-screen-xl mx-auto " aria-label="Global">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8 max-w-screen-xl mx-auto "
+            aria-label="Global"
+          >
             <div className="flex lg:flex-1">
               <h1 className={(gothic.className, 'text-xl font-bold')}>
                 <Link href="/">Vicky&apos;s blog</Link>
@@ -40,7 +47,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </header>
         <div className="max-w-screen-xl mx-auto p-6 lg:px-8">{children}</div>
         <div className="fixed inset-x-0 bottom-0 text-center p-3 bg-black text-white">
-          <p className="text-sm">Don&apos;t forget to CODE you DREAM | All Right Reserved.</p>
+          <p className="text-sm">
+            Don&apos;t forget to CODE you DREAM | All Right Reserved.
+          </p>
         </div>
       </body>
     </html>
