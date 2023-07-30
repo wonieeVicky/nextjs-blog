5ab54d5129972eb85b15a705c468c52efa1e0751
wonieeVicky <hwfongfing@gmail.com>
2023-07-30 16:06:19 +0900

    chore: fix About page description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,21 +17,13 @@ export default function About() {
           비즈니스의 가치를 구현하는 프론트엔드 개발자
         </div>
         <h3 className="text-xl font-semibold my-3 mt-5">Career</h3>
-        <div className="text-sm">(주)유니드컴즈 (2018 ~ Now)</div>
+        <div className="text-sm">UneedComms.Inc (2018 ~ Now)</div>
         <h3 className="text-xl font-semibold my-3 mt-5">Skill</h3>
         <div className="text-sm">
           React, Node, Next.js, Typescript, Javascript, Three.js
         </div>
         <div className="text-sm">
           Git, VsCode, IntelliJ, Sourcetree, Webpack
-        </div>
-        <h3 className="text-xl font-semibold my-3 mt-5">
-          Education & Certificate
-        </h3>
-        <div className="text-sm">한국방송통신대학교 컴퓨터과학과 (2023.08)</div>
-        <div className="text-sm">
-          정보처리기사, 웹디자인기능사, 그래픽기술자격(GTQ),
-          그래픽기술자격일러스트(GTQi)
         </div>
       </div>
     </main>
