2ce438194caea608b9badbd7677cc335051d5cc9
wonieeVicky <hwfongfing@gmail.com>
2023-07-30 15:12:09 +0900

    fix: Me intro description

--- src/app/Me.tsx
+++ src/app/Me.tsx
@@ -6,10 +6,17 @@ export default function Me() {
   return (
     <div className="p-8 text-center w-full">
       <div className="border w-52 h-52 border-radius overflow-hidden rounded-full mb-3 inline-block">
-        <Image src={meImage} alt="me image" className="relative -top-3" priority />
+        <Image
+          src={meImage}
+          alt="me image"
+          className="relative -top-3"
+          priority
+        />
       </div>
       <h2 className="text-2xl font-bold mb-1">Hi! I&apos;m Vicky 👩🏻‍💻</h2>
-      <p className="text-sm mb-3">Creating delightful experiences fills me with inspiration</p>
+      <p className="text-sm mb-3">
+        Boundless inspiration in crafting delightful experiences
+      </p>
       <button className="rounded-full bg-orange-500 p-1 px-3 text-xs font-semibold text-white">
         <Link href="/contact">Contact Me</Link>
       </button>
