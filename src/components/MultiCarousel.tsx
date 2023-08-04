93811bcff440c2cb4d43afcebbe3045a0ae07261
wonieeVicky <hwfongfing@gmail.com>
2023-08-04 23:33:28 +0900

    feat: MultiCarousel 영역만 일부 CSR 컴포넌트로 노출되도록 구조 변경

---
+++ src/components/MultiCarousel.tsx
@@ -0,0 +1,30 @@
+﻿'use client';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 640 },
+    items: 3
+  },
+  mobile: {
+    breakpoint: { max: 640, min: 0 },
+    items: 2
+  }
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default async function MultiCarousel({ children }: Props) {
+  return (
+    <Carousel responsive={responsive} itemClass="m-2" infinite autoPlay>
+      {children}
+    </Carousel>
+  );
+}
