3745ead08b7c3e52563d91056940defdc249f6a9
wonieeVicky <hwfongfing@gmail.com>
2023-08-05 23:40:30 +0900

    feat: FilterablePosts 스타일 추가

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -6,11 +6,17 @@
 
 export default function Categories({ categories, selected, onClick }: Props) {
   return (
-    <section>
-      <h2>Cateogory</h2>
+    <section className="text-center p-4">
+      <h2 className="text-lg fotn-bold border-b border-orange-500 mb-2">Cateogory</h2>
       <ul>
         {categories.map((category) => (
-          <li key={category} onClick={() => onClick(category)}>
+          <li
+            className={`cursor-pointer hover:text-orange-500 ${
+              category === selected && 'text-orange-600'
+            } transition-colors duration-300 ease-in-out`}
+            key={category}
+            onClick={() => onClick(category)}
+          >
             {category}
           </li>
         ))}
