761d83efc57bbae7e6ba572c6f78c36210f9d484
wonieeVicky <hwfongfing@gmail.com>
2023-08-05 23:36:49 +0900

    chore: 미사용 주석 제거

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -14,12 +14,7 @@ export default function Toast({ isSuccess }: { isSuccess: boolean }) {
       >
         <div className="flex">
           <div className="w-8 h-8 border-radius overflow-hidden rounded-full shadow-lg">
-            <Image
-              src={meImage}
-              alt="me image"
-              className="relative -top-1"
-              priority
-            />
+            <Image src={meImage} alt="me image" className="relative -top-1" priority />
           </div>
           <div className="ml-3 max-w-xs text-sm text-left font-normal">
             <div className="text-sm font-semibold mb-1">Vicky</div>
@@ -29,33 +24,6 @@ export default function Toast({ isSuccess }: { isSuccess: boolean }) {
                 : '메일 전송이 실패했습니다. 다시 시도해주세요.'}
             </div>
           </div>
-          {/* <button
-            type="button"
-            className={`ml-auto -mx-1.5 -my-1.5 justify-center items-center flex-shrink-0 rounded-lg focus:ring-0 p-1.5  inline-flex h-8 w-8 ${
-              isSuccess
-                ? 'text-emerald-700 hover:text-emerald-900'
-                : 'text-red-700 hover:text-red-900'
-            }`}
-            data-dismiss-target="#toast-message-cta"
-            aria-label="Close"
-          >
-            <span className="sr-only">Close</span>
-            <svg
-              className="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-          </button> */}
         </div>
       </div>
     </>
