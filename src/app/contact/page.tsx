3f75dc713a5d975594580e468a24e854a0740cc8
wonieeVicky <hwfongfing@gmail.com>
2023-08-06 17:50:30 +0900

    feat: contactPage 재조립

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
-﻿import SendEmail from '@/components/SendEmail';
+﻿import ContactForm from '@/components/ContactForm';
 import { Metadata } from 'next';
-import Link from 'next/link';
-import { SiGithub, SiNotion } from 'react-icons/si';
+import { SiGithub, SiLinkedin, SiNotion } from 'react-icons/si';
 
 export function generateMetadata(): Metadata {
   return {
@@ -10,30 +9,41 @@ export function generateMetadata(): Metadata {
   };
 }
 
+const LINKS = [
+  {
+    icon: <SiGithub />,
+    url: '//github.com/wonieeVicky'
+  },
+  {
+    icon: <SiNotion />,
+    url: '//fongfing.notion.site/Vicky-s-FE-Engineering-Wiki-d7e660205c0047118a78d664b07418fd'
+  },
+  {
+    icon: <SiLinkedin />,
+    url: '//www.linkedin.com/in/%ED%98%9C%EC%9B%90-%EC%B5%9C-b97a45285/'
+  }
+];
+
 export default function ContactPage() {
   return (
-    <section className="flex flex-col items-center justify-between">
-      <div className="w-full text-center">
-        <h3 className="text-2xl font-bold my-3">Contact Me</h3>
-        <div className="text-sm">hwfongfing@gmail.com</div>
-        <div className="flex justify-center my-5 text-2xl hover:text">
-          <Link
-            href="//github.com/wonieeVicky"
+    <section className="flex flex-col items-center">
+      <h2 className="text-2xl font-bold my-3">Contact Me</h2>
+      <p>hwfongfing@gmail.com</p>
+      <ul className="flex gap-4 my-5">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
             target="_blank"
-            className="mx-1 transition ease-in-out duration-300 hover:text-orange-600"
+            rel="noreferrer"
+            className="text-2xl hover:text-orange-400"
           >
-            <SiGithub />
-          </Link>
-          <Link
-            href="//fongfing.notion.site/Vicky-s-FE-Engineering-Wiki-d7e660205c0047118a78d664b07418fd"
-            target="_blank"
-            className="mx-1 transition ease-in-out duration-200 hover:text-orange-600"
-          >
-            <SiNotion />
-          </Link>
-        </div>
-        <SendEmail />
-      </div>
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-2xl font-bold my-3">Or Send me an email📮</h2>
+      <ContactForm />
     </section>
   );
 }
