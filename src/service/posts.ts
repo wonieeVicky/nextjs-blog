cd2a2df233354a1d3e85db5413a6aa8f5a5a9589
wonieeVicky <hwfongfing@gmail.com>
2023-08-08 23:39:09 +0900

    refactor: getPosts service 함수에 cache 적용

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 ﻿import path from 'path';
 import { promises as fs } from 'fs';
+import { cache } from 'react';
 
 export type Post = {
   title: string;
@@ -16,13 +17,16 @@ export type PostData = Post & {
   prev: Post | null;
 };
 
-export async function getPosts(): Promise<Post[]> {
+// 중복 방지 처리 - cache는 동일한 인자로 호출하면 캐시된 값을 반환한다.
+// 서버가 동작하는 모든 구간에서 캐시 적용하는 것이 아닌 한번 렌더링 되는 사이클에 한해서만 캐시 적용
+// 다른 페이지로 이동 시 캐시된 값은 사라지고 새로운 값을 불러와 값을 캐시한다.
+export const getPosts = cache(async () => {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
   return await fs
     .readFile(filePath, 'utf-8')
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
-}
+});
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getPosts().then((posts) => posts.filter((post) => post.featured));
